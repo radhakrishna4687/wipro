9719935532da5e1fad3609a55683aafc745e6106
siva <sivakmr352@gmail.com>
2019-12-15 14:31:35 +0000

    webhook commit

--- one.sh
+++ one.sh
@@ -2,8 +2,21 @@ hi
 biotany
 hi zooo
 hi chemistry
-<<<<<<< HEAD
 jenkins
-=======
+FROM ubuntu:12.04
 
->>>>>>> fc1fd7812a5e430315da87cefc05bada0533ce2e
+MAINTAINER Valaxy Technologies valaxytech@gmail.com
+
+LABEL version="1.1.0" \
+      app_name="Training registration application" \
+	  release_date="9-Sep-2018"
+RUN apt-get update && apt-get install -y apache2 && apt-get clean
+
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+
+EXPOSE 80
+
+COPY index.html /var/www/html
+CMD ["/usr/sbin/apache2", "-D", "FOREGROUND"]
